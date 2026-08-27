15a94c46e9e207d8c066887b5fa37d5ce889d1d3
greatmanvictor789-jpg <greatmanvictor789@gmail.com>
2026-08-27 21:25:45 +0100

    implemented home page

---
+++ script.js
@@ -0,0 +1,30 @@
+// Nav link highlighting
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.header-main-nav ul li a');
+    
+    // Highlight the current page on load
+    highlightCurrentPage(navLinks);
+    
+    // Add click event to each nav link
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            // Remove selected class from all links
+            navLinks.forEach(l => l.classList.remove('selected'));
+            
+            // Add selected class to clicked link
+            this.classList.add('selected');
+        });
+    });
+});
+
+// Function to highlight the current page
+function highlightCurrentPage(navLinks) {
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    
+    navLinks.forEach(link => {
+        const href = link.getAttribute('href');
+        if (href === currentPage || (currentPage === '' && href === 'index.html')) {
+            link.classList.add('selected');
+        }
+    });
+}
